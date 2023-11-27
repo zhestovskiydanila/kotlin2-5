9185dfaaa5af587ebe25df8e24f7ef7681b49cac
zhestovskiydanila <zhestovskiy.danila@mail.ru>
2023-11-27 20:18:34 +0300

    init

---
+++ app/src/main/java/ru/myitschool/lab23/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.myitschool.lab23
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.myitschool.lab23.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+    }
+}
